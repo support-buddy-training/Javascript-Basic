7dee154d2184eb381999012545bda57ab37b6ef7
Dang Lan <danglan910d1@gmail.com>
2022-10-03 15:24:08 +0700

    refactor: add lessons to part 10

--- main.test.js
+++ main.test.js
@@ -1,8 +1,8 @@
 // const sum = require("./main");
 
 // Imprort ES6:
-import { sum, multiply } from "./main";
+import { sum, multiply } from './main';
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 3', () => {
+	expect(sum(1, 2)).toBe(3);
 });
