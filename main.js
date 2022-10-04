6610adc5c603423905f13e1a1066a77904813cf3
Dang Lan <danglan910d1@gmail.com>
2022-10-04 17:59:30 +0700

    refactor: add file main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// Part 10 lesson 2: Setup Jest framework
+
+// Test example ES5:
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
+
+// ES6
+export function sum(a, b) {
+	return a + b;
+}
+
+export function multiply(a, b) {
+	return a * b;
+}
