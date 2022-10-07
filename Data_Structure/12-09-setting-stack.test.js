76410c70116b5e58d6613b51300158a5606c667c
Dang Lan <danglan910d1@gmail.com>
2022-10-07 17:59:56 +0700

    refactor: add file 12-09-setting-stack.test.js

---
+++ Data_Structure/12-09-setting-stack.test.js
@@ -0,0 +1,69 @@
+// Part 12 lesson 9: Setting Stack
+
+function createStack() {
+	const stack = [];
+
+	function getTop() {
+		return stack.length > 0 ? stack[stack.length - 1] : undefined;
+	}
+
+	function getSize() {
+		return stack.length;
+	}
+
+	function isEmpty() {
+		return stack.length === 0;
+	}
+
+	function push(data) {
+		stack.push(data);
+	}
+
+	function pop() {
+		return stack.pop();
+	}
+
+	return {
+		getTop,
+		getSize,
+		isEmpty,
+		push,
+		pop,
+	};
+}
+
+describe('createStack()', () => {
+	test('All in one!', () => {
+		// empty stack
+		const stack = createStack();
+		expect(stack.getSize()).toBe(0);
+		expect(stack.isEmpty()).toBe(true);
+		expect(stack.getTop()).toBeUndefined(true);
+
+		// push: 4
+		stack.push(4);
+		expect(stack.getSize()).toBe(1);
+		expect(stack.isEmpty()).toBe(false);
+		expect(stack.getTop()).toBe(4);
+
+		// push 2
+		stack.push(2);
+		expect(stack.getSize()).toBe(2);
+		expect(stack.isEmpty()).toBe(false);
+		expect(stack.getTop()).toBe(2);
+
+		// pop() --> 2
+		const two = stack.pop();
+		expect(two).toBe(2);
+		expect(stack.getSize()).toBe(1);
+		expect(stack.isEmpty()).toBe(false);
+		expect(stack.getTop()).toBe(4);
+
+		// pop() --> 2
+		const four = stack.pop();
+		expect(four).toBe(4);
+		expect(stack.getSize()).toBe(0);
+		expect(stack.isEmpty()).toBe(true);
+		expect(stack.getTop()).toBeUndefined();
+	});
+});
